98e78549024d5bbfd75001a85e70b29897f3692c
Aditikarn07 <108510995+Aditikarn07@users.noreply.github.com>
2022-08-07 21:24:31 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,77 @@
+let allMusic = [
+
+    {
+        name : " Janam Janam Ka Saath Hai ",
+        artist : "Mohd Rafi, Lata Mangeshkar",
+        img : "Music-1",
+        src : "Music-1"
+    },
+    {
+        name : "Dil To Pagal hai",
+        artist : "Udit Narayan, Lata Mangeshkar",
+        img : "Music-2",
+        src :  "Music-2"
+    },
+    {
+        name : "Yeh Dil Tum Bin",
+        artist : "Mohd Rafi, Lata Mangeshkar",
+        img : "Music-3",
+        src : "Music-3"
+    },
+    {
+        name : "O Palanhare",
+        artist : "Udit Narayan, Lata Mangeshkar",
+        img : "Music-4",
+        src : "Music-4"
+    },
+    {
+        name : "Tujhe Dekha To Ye Jana Sanam",
+        artist : "Kumar Sanu, Lata Mangeshkar",
+        img : "Music-5",
+        src : "Music-5"
+    },
+    {
+        name : "Do Pal Ruka",
+        artist : "Sonu Nigam, Lata Mangeshkar",
+        img : "Music-6",
+        src : "Music-6"
+    },
+    {
+        name : " Ek Pyar Ka Nagma Hai",
+        artist : "Mukesh, Lata Mangeshkar",
+        img : "Music-7",
+        src : "Music-7"
+    },
+
+    {
+        name : "Lag Ja Gale",
+        artist : "Lata Mangeshkar",
+        img : "Music-8",
+        src : "Music-8"
+    },
+    {
+        name : "Jhilmil Sitaro Ka",
+        artist : "Mohd Rafi, Lata Mangeshkar",
+        img : "Music-9",
+        src : "Music-9"
+    },
+    {
+        name : "Gore Rang Pe",
+        artist : "kishore Kumar, Lata Mangeshkar",
+        img : "Music-10",
+        src : "Music-10"
+    },
+    {
+        name : "Salam-E-Ishq",
+        artist : "Kishore Kumar, Lata Mangeshkar",
+        img : "Music-11",
+        src : "Music-11"
+    },
+    {
+        name : "Tere Chehre Se Nazar",
+        artist : "Kishore Kumar, Lata Mangeshkar",
+        img : "Music-12",
+        src : "Music-12"
+    },
+    
+];
